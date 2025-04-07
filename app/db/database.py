58db4856207f38dde0e1c2ba8d9624d885fa1747
Daniyal <daniyaljob@gmail.com>
2025-04-07 18:23:21 +0300

    added env variables

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,10 @@
 ﻿from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import settings
 
-DATABASE_URL = "sqlite:///./blog.db"
+#DEFAULT_DATABASE_URL = "sqlite:///./blog.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
