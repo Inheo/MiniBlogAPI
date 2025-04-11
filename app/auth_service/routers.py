a26fd959f65a73d2f11ae04c08d9ed4f814a2872
Daniyal <daniyaljob@gmail.com>
2025-04-11 16:32:47 +0300

    fixed token class use

--- app/auth_service/routers.py
+++ app/auth_service/routers.py
@@ -1,12 +1,14 @@
 ﻿from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+
+from app.auth_service.schemas import Token
 from app.db.database import get_db
 from app.auth_service import models, schemas, security
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
-@router.post("/register", response_model=schemas.Token)
+@router.post("/register", response_model=Token)
 def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     existing = db.query(models.User).filter(models.User.email == user.email).first()
     if existing:
@@ -19,13 +21,13 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(db_user)
 
     token = security.create_access_token({"sub": str(db_user.id)})
-    return {"access_token": token, "token_type": "bearer"}
+    return Token(access_token=token)
 
-@router.post("/token", response_model=schemas.Token)
+@router.post("/token", response_model=Token)
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == form_data.username).first()
     if not user or not security.verify_password(form_data.password, user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
     token = security.create_access_token({"sub": str(user.id)})
-    return {"access_token": token, "token_type": "bearer"}
+    return Token(access_token=token)
--- app/auth_service/schemas.py
+++ app/auth_service/schemas.py
@@ -6,4 +6,4 @@ class UserCreate(BaseModel):
 
 class Token(BaseModel):
     access_token: str
-    token_type: str
+    token_type: str = "bearer"
