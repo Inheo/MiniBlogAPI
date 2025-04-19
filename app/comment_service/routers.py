ae22ab7bf2b47ecd0e7c51428181b5c2e4168417
Daniyal <daniyaljob@gmail.com>
2025-04-19 17:13:36 +0300

    added comments router

---
+++ app/comment_service/routers.py
@@ -0,0 +1,77 @@
+﻿from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from app.comment_service import schemas, models
+from app.db.database import get_db
+from app.auth_service.security import get_current_auth_user
+from app.auth_service import models as auth_models
+from app.post_service.models import Post
+
+router = APIRouter(prefix="/comments", tags=["Comments"])
+
+@router.get("/post/{post_id}", response_model=list[schemas.Comment])
+def get_comments_for_post(post_id: int, db: Session = Depends(get_db)):
+    return db.query(models.Comment).where(models.Comment.post_id == post_id).all()
+
+@router.post("/post/{post_id}", response_model=schemas.Comment)
+def create_comment_for_post(
+    post_id: int,
+    comment_data: schemas.CommentCreate,
+    db: Session = Depends(get_db),
+    current_user: auth_models.User = Depends(get_current_auth_user)
+):
+    post = db.query(Post).filter(Post.id == post_id).first()
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+    comment = models.Comment(
+        content=comment_data.content,
+        post_id=post.id,
+        user_id=current_user.id
+    )
+
+    db.add(comment)
+    db.commit()
+    db.refresh()
+    return comment
+
+
+@router.put("/{comment_id}", response_model=schemas.Comment)
+def update_comment(
+    comment_id: int,
+    comment_data: schemas.CommentCreate,
+    db: Session = Depends(get_db),
+    current_user: auth_models.User = Depends(get_current_auth_user)
+):
+
+    comment = get_comment_by_user(comment_id, db, current_user)
+
+    comment.content = comment_data.content
+    db.commit()
+    db.refresh()
+    return comment
+
+
+@router.delete("/comment_id", status_code=status.HTTP_204_NO_CONTENT)
+def delete_comment(
+    comment_id: int,
+   db: Session = Depends(get_db),
+   current_user: auth_models.User = Depends(get_current_auth_user)
+):
+    comment = get_comment_by_user(comment_id, db, current_user)
+
+    db.delete(comment)
+    db.commit()
+
+
+def get_comment_by_user(comment_id:int, db: Session, current_user: auth_models.User) -> models.Comment:
+    comment = db.query(models.Comment).filter(models.Comment.id == comment_id).first()
+
+    if not comment:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found")
+
+    if not comment.user_id == current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not your comment")
+
+    return comment
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 ﻿from fastapi import FastAPI
 from app.auth_service import routers as auth_router
 from app.post_service import routers as post_router
+from app.comment_service import routers as comment_router
 from app.db import database
 
 app = FastAPI(title="Mini Blog API")
@@ -8,3 +9,4 @@ database.Base.metadata.create_all(bind=database.engine)
 
 app.include_router(auth_router.router)
 app.include_router(post_router.router)
+app.include_router(comment_router.router)
