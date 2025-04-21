9c781499801ef60fc40a8fb7bd7329f66d6e9033
Daniyal <daniyaljob@gmail.com>
2025-04-21 18:32:46 +0300

    add auth from Swagger Autorize with OAuth2PasswordRequestForm

--- app/auth_service/routers.py
+++ app/auth_service/routers.py
@@ -1,5 +1,5 @@
 ﻿from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import HTTPBearer
+from fastapi.security import HTTPBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.auth_service import schemas, models
@@ -32,13 +32,13 @@ def register_user(user_data: schemas.UserCreate, db: Session = Depends(get_db)):
     db.add(user)
     db.commit()
     db.refresh(user)
-    return user
+    return generate_token(user)
 
 
 @router.post("/token", response_model=schemas.Token)
-def login(user_credentials: schemas.UserCreate, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
-    if not user or not verify_password(user_credentials.password, user.hashed_password):
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid email or password"
