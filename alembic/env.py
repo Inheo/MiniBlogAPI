34528d81a6d10d68763837543c0e3b825d82f571
Daniyal <daniyaljob@gmail.com>
2025-04-23 11:30:41 +0300

    imported all models

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,9 @@ from alembic import context
 
 from app.config import settings
 from app.db.database import Base
+from app.auth_service import models as auth_models
+from app.post_service import models as post_models
+from app.comment_service import models as comment_models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
