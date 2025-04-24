cd42485f910181207f089b65261560515d1823e3
Daniyal <daniyaljob@gmail.com>
2025-04-24 13:55:38 +0300

    switched sqlite to postgresql

--- app/db/database.py
+++ app/db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
 from app.config import settings
 
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 def get_db():
