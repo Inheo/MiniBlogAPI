95060e33957a8bb855b8156edc9df87fd962f808
Daniyal <daniyaljob@gmail.com>
2025-05-04 21:39:35 +0300

    refactor: added service layer for comment

--- app/comment/routers.py
+++ app/comment/routers.py
@@ -1,13 +1,18 @@
-﻿from fastapi import APIRouter, Depends, HTTPException, status
+﻿from fastapi import APIRouter, Depends, status
 from fastapi.security import HTTPBearer
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
 
-from app.comment import schemas, models
+from app.comment.schemas import Comment, CommentCreate
 from app.db.database import get_async_session
 from app.auth.security import get_current_auth_user
-from app.auth import models as auth_models
-from app.post.models import Post
+from app.auth.models import User
+
+from .services import (
+    fetch_comments_by_post_id,
+    add_comment_for_post,
+    update_user_comment,
+    remove_comment_by_user
+)
 
 router = APIRouter(
     prefix="/comments",
@@ -15,73 +20,37 @@ router = APIRouter(
     dependencies=[Depends(HTTPBearer(auto_error=False))]
 )
 
-@router.get("/post/{post_id}", response_model=list[schemas.Comment])
+@router.get("/post/{post_id}", response_model=list[Comment])
 async def get_comments_for_post(post_id: int, session: AsyncSession = Depends(get_async_session)):
-    result = await session.execute(select(models.Comment).where(models.Comment.post_id == post_id))
-    return result.scalars().all()
+    return await fetch_comments_by_post_id(post_id, session)
 
-@router.post("/post/{post_id}", response_model=schemas.Comment)
+@router.post("/post/{post_id}", response_model=Comment)
 async def create_comment_for_post(
     post_id: int,
-    comment_data: schemas.CommentCreate,
+    comment_data: CommentCreate,
     session: AsyncSession = Depends(get_async_session),
-    current_user: auth_models.User = Depends(get_current_auth_user)
+    current_user: User = Depends(get_current_auth_user)
 ):
-    post = await session.get(Post, post_id)
 
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-
-    if comment_data.parent_id:
-        parent = await session.get(models.Comment, comment_data.parent_id)
-        if not parent or parent.post_id != post_id:
-            raise HTTPException(status_code=400, detail="Invalid parent comment")
+    return await add_comment_for_post(post_id, comment_data, session, current_user.id)
 
 
-    comment = models.Comment(
-        content=comment_data.content,
-        post_id=post.id,
-        user_id=current_user.id,
-        parent_id = comment_data.parent_id
-    )
-
-    session.add(comment)
-    await session.flush()
-    return comment
-
-
-@router.put("/{comment_id}", response_model=schemas.Comment)
+@router.put("/{comment_id}", response_model=Comment)
 async def update_comment(
     comment_id: int,
-    comment_data: schemas.CommentCreate,
+    comment_data: CommentCreate,
     session: AsyncSession = Depends(get_async_session),
-    current_user: auth_models.User = Depends(get_current_auth_user)
+    current_user: User = Depends(get_current_auth_user)
 ):
-
-    comment = get_comment_by_user(comment_id, session, current_user)
-
-    comment.content = comment_data.content
-    return comment
+    return await update_user_comment(comment_id, comment_data, session, current_user.id)
 
 
 @router.delete("/{comment_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_comment(
     comment_id: int,
     session: AsyncSession = Depends(get_async_session),
-   current_user: auth_models.User = Depends(get_current_auth_user)
+    current_user: User = Depends(get_current_auth_user)
 ):
-    comment = get_comment_by_user(comment_id, session, current_user)
-
-    await session.delete(comment)
+    await remove_comment_by_user(comment_id, session, current_user.id)
 
 
-async def get_comment_by_user(comment_id:int, session: AsyncSession, current_user: auth_models.User) -> models.Comment:
-    comment: models.Comment | None = await session.get(models.Comment, comment_id)
-
-    if not comment:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found")
-
-    if not comment.user_id == current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not your comment")
-
-    return comment
---
+++ app/comment/services.py
@@ -0,0 +1,73 @@
+﻿from fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from app.post.models import Post
+from .schemas import CommentCreate
+from .models import Comment
+
+
+async def fetch_comments_by_post_id(post_id: int, session: AsyncSession) -> [Comment]:
+    result = await session.execute(select(Comment).where(Comment.post_id == post_id))
+    return result.scalars().all()
+
+
+async def add_comment_for_post(
+    post_id: int,
+    comment_data: CommentCreate,
+    session: AsyncSession,
+    current_user_id: int
+) -> Comment:
+    post = await session.get(Post, post_id)
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+    if comment_data.parent_id:
+        parent = await session.get(Comment, comment_data.parent_id)
+        if not parent or parent.post_id != post_id:
+            raise HTTPException(status_code=400, detail="Invalid parent comment")
+
+
+    comment = Comment(
+        content=comment_data.content,
+        post_id=post.id,
+        user_id=current_user_id,
+        parent_id = comment_data.parent_id
+    )
+
+    session.add(comment)
+    await session.flush()
+    return comment
+
+
+async def update_user_comment(
+    comment_id: int,
+    comment_data: CommentCreate,
+    session: AsyncSession,
+    current_user_id: int
+) -> Comment:
+    comment = await get_comment_by_user(comment_id, session, current_user_id)
+    comment.content = comment_data.content
+    return comment
+
+
+async def remove_comment_by_user(
+    comment_id: int,
+    session: AsyncSession,
+    current_user_id: int
+):
+    comment = await get_comment_by_user(comment_id, session, current_user_id)
+    await session.delete(comment)
+
+
+async def get_comment_by_user(comment_id:int, session: AsyncSession, current_user_id: int) -> Comment:
+    comment: Comment | None = await session.get(Comment, comment_id)
+
+    if not comment:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found")
+
+    if not comment.user_id == current_user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not your comment")
+
+    return comment
