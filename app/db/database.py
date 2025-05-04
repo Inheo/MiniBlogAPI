b9ae4dec7a774941bc608b7bd51c886a1f06efef
Daniyal <daniyaljob@gmail.com>
2025-05-04 16:47:33 +0300

    refactor: deleted index true from primary key

--- app/db/database.py
+++ app/db/database.py
@@ -17,4 +17,4 @@ class Base(DeclarativeBase):
 class BaseWithId(Base):
     __abstract__ = True
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
