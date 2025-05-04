d0310ce10994897819886ac3795c3f214fcc2607
Daniyal <daniyaljob@gmail.com>
2025-05-04 21:07:10 +0300

    reafactor: added service layer for auth

--- app/auth/routers.py
+++ app/auth/routers.py
@@ -1,15 +1,17 @@
-﻿from fastapi import APIRouter, Depends, HTTPException, status
+﻿from fastapi import APIRouter, Depends
 from fastapi.security import HTTPBearer, OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
 
-from app.auth import schemas, models
 from app.db.database import get_async_session
-from app.auth.security import (
-    hash_password,
-    verify_password,
-    generate_token,
+from .schemas import UserCreate, Token
+from .models import User
+from .services import (
+    register_new_user,
+    login_new
+)
+from .security import (
     get_current_auth_user_for_refresh,
+    generate_token
 )
 
 router = APIRouter(
@@ -18,38 +20,19 @@ router = APIRouter(
     dependencies=[Depends(HTTPBearer(auto_error=False))]
 )
 
-@router.post("/register", response_model=schemas.Token)
-async def register_user(user_data: schemas.UserCreate, session: AsyncSession = Depends(get_async_session)):
-    result = await session.execute(select(models.User).where(models.User.email == user_data.email))
-    existing_user = result.scalars().first()
-    if existing_user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="User with this email already exists"
-        )
-    user = models.User(
-        email=user_data.email,
-        hashed_password=hash_password(user_data.password),
-    )
-    session.add(user)
-    await session.flush()
-    return generate_token(user)
+@router.post("/register", response_model=Token)
+async def register_user(user_data: UserCreate, session: AsyncSession = Depends(get_async_session)):
+    return await register_new_user(user_data, session)
 
 
-@router.post("/token", response_model=schemas.Token)
+@router.post("/token", response_model=Token)
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), session: AsyncSession = Depends(get_async_session)):
-    result = await session.execute(select(models.User).where(models.User.email == form_data.username))
-    user = result.scalars().first()
-    if not user or not verify_password(form_data.password, user.hashed_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid email or password"
-        )
-    return generate_token(user)
+    user_data = UserCreate(email=form_data.username, password=form_data.password)
+    return await login_new(user_data, session)
 
 
-@router.post("/refresh", response_model=schemas.Token)
+@router.post("/refresh", response_model=Token)
 def refresh_token(
-    current_user: models.User = Depends(get_current_auth_user_for_refresh)
+    current_user: User = Depends(get_current_auth_user_for_refresh)
 ):
     return generate_token(current_user)
---
+++ app/auth/services.py
@@ -0,0 +1,46 @@
+﻿from pydantic import EmailStr
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from fastapi import HTTPException, status
+
+from .schemas import UserCreate, Token
+from .models import User
+from .security import (
+    hash_password,
+    generate_token,
+    verify_password
+)
+
+
+async def get_user_by_email(email: EmailStr, session: AsyncSession) -> User:
+    result = await session.execute(select(User).where(User.email == email))
+    return result.scalars().first()
+
+
+async def register_new_user(user_data: UserCreate, session: AsyncSession) -> Token:
+    existing_user = await get_user_by_email(user_data.email, session)
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with this email already exists"
+        )
+
+    user = User(
+        email=user_data.email,
+        hashed_password=hash_password(user_data.password),
+    )
+
+    session.add(user)
+    await session.flush()
+    return generate_token(user)
+
+
+async def login_new(user_data: UserCreate, session: AsyncSession) -> Token:
+    user = await get_user_by_email(user_data.email, session)
+
+    if not user or not verify_password(user_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password"
+        )
+    return generate_token(user)
