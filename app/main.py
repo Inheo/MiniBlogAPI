90157dff3e85ef902071c283c0ce8f402b591d2e
Daniyal <daniyaljob@gmail.com>
2025-05-11 21:18:06 +0300

    added notifications router to main

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.auth import routers as auth_router
 from app.post import routers as post_router
 from app.comment import routers as comment_router
+from app.notification import routers as notification_router
 from app.db import database
 
 
@@ -17,3 +18,4 @@ app = FastAPI(title="Mini Blog API", lifespan=lifespan)
 app.include_router(auth_router.router)
 app.include_router(post_router.router)
 app.include_router(comment_router.router)
+app.include_router(notification_router.router)
