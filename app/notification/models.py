f0c8c4978d46dbce08cddc3759e04220b3292337
Daniyal <daniyaljob@gmail.com>
2025-05-11 21:17:42 +0300

    fixed bug of notifications model

--- app/notification/models.py
+++ app/notification/models.py
@@ -1,6 +1,6 @@
 ﻿from datetime import datetime
 
-from sqlalchemy import ForeignKey, Integer, String, Bool
+from sqlalchemy import ForeignKey, Integer, String, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.database import Base
@@ -10,7 +10,7 @@ class Notification(Base):
 
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
     message: Mapped[str] = mapped_column(String)
-    created_at: Mapped[datetime] = mapped_column(default_factory=datetime.now)
-    is_read: Mapped[bool] = mapped_column(Bool, default=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+    is_read: Mapped[bool] = mapped_column(default=False)
 
     user = relationship("User", back_populates="notifications")
